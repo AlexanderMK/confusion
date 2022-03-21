5052ffc72c634b36e57024c1d00c3a1bc7e70e77
Alexander MK <alexandermkdev@gmail.com>
2022-03-21 15:12:12 +0300

    placed onClick events

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -6,7 +6,33 @@ class Menu extends Component {
         super(props);
 
         this.state = {
+            selectedDish: null
             
+        }
+    }
+
+    onDishSelect(dish) {
+        this.setState({ selectedDish: dish})
+    }
+
+    renderDish(dish) {
+        if (dish != null) {
+            return(
+                <Card>
+                    <CardImg width="100%" object src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+
+                </Card>
+
+            );
+        }
+        else {
+            return(
+                <div></div>
+            )
         }
     }
 
@@ -14,7 +40,7 @@ class Menu extends Component {
         const menu = this.props.dishes.map((dish) => {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card>
+                <Card onClick={() => this.onDishSelect(dish)}>
                   
                       <CardImg width="100%" object src={dish.image} alt={dish.name} />
                   
@@ -29,10 +55,11 @@ class Menu extends Component {
 
         return (
           <div className="container">
+            <div className="row">         
+              {menu}
+            </div>
             <div className="row">
-              
-                  {menu}
-              
+                {this.renderDish(this.state.selectedDish)}
             </div>
           </div>
         );
